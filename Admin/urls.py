c024c8566b85643df79c71b435eccc36f397549a
AntonVanke <fjs0801@gmail.com>
2020-12-22 07:40:43 +0800

    增加后台管理页面

--- Admin/urls.py
+++ Admin/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from Admin import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="user_index"),
     path("login.html", views.login, name="user_login"),
     path("register.html", views.register, name="user_register"),
     path("forget.html", views.forget, name="user_forget"),
--- Pyecho/settings.py
+++ Pyecho/settings.py
@@ -102,9 +102,17 @@ WSGI_APPLICATION = 'Pyecho.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pyecho',  # 数据库
+        'USER': 'root',  # 用户
+        'PASSWORD': '11111111a',  # 密码
+        'HOST': '127.0.0.1',  # 地址
+
     }
 }
 
@@ -146,4 +154,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "statics")
 ]
-
