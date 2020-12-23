55bdde82d679d682b4af4953303cfc7cc291de12
AntonVanke <fjs0801@gmail.com>
2020-12-23 22:49:18 +0800

    前端部分

--- Page/views.py
+++ Page/views.py
@@ -4,4 +4,60 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Page.ok!")
+    site = {
+        'url': 'http://127.0.0.1:8000/',
+        'title': '易部落阁',
+    }
+    page = {
+        'title': site['title'] + ' 首页',
+    }
+    articles = [
+        {
+            'author': 'FJS',
+            'author_url': 'http://127.0.0.1:8000/FJS',
+            'article_time': '2020 年 12 月 21 日',
+            'meta_url': 'http://127.0.0.1:8000/default',
+            'meta': '默认分类',
+            'comment_url': 'http://127.0.0.1:8000/comment',
+            'comment_count': '11',
+            'abstract': '11',
+        },
+          {
+            'author': 'FJS',
+            'author_url': 'http://127.0.0.1:8000/FJS',
+            'article_time': '2020 年 12 月 21 日',
+            'meta_url': 'http://127.0.0.1:8000/default',
+            'meta': '默认分类',
+            'comment_url': 'http://127.0.0.1:8000/comment',
+            'comment_count': '11',
+        },
+          {
+            'author': 'FJS',
+            'author_url': 'http://127.0.0.1:8000/FJS',
+            'article_time': '2020 年 12 月 21 日',
+            'meta_url': 'http://127.0.0.1:8000/default',
+            'meta': '默认分类',
+            'comment_url': 'http://127.0.0.1:8000/comment',
+            'comment_count': '11',
+        },
+          {
+            'author': 'FJS',
+            'author_url': 'http://127.0.0.1:8000/FJS',
+            'article_time': '2020 年 12 月 21 日',
+            'meta_url': 'http://127.0.0.1:8000/default',
+            'meta': '默认分类',
+            'comment_url': 'http://127.0.0.1:8000/comment',
+            'comment_count': '11',
+        },
+        {
+            'author': 'FJS',
+            'author_url': 'http://127.0.0.1:8000/FJS',
+            'article_time': '2020 年 12 月 21 日',
+            'meta_url': 'http://127.0.0.1:8000/default',
+            'meta': '默认分类',
+            'comment_url': 'http://127.0.0.1:8000/comment',
+            'comment_count': '11',
+        },
+
+    ]
+    return render(request, "Page/test.html", locals())
--- Pyecho/settings.py
+++ Pyecho/settings.py
@@ -54,9 +54,8 @@ ROOT_URLCONF = 'Pyecho.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
-        'APP_DIRS': True,
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
